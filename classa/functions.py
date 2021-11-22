81252a61dee5d0573e668d9fe5d4d757d1d18790
mghoneem10 <mghoneem10@gmail.com>
2021-11-22 10:31:07 +0000

    functions

---
+++ classa/functions.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+import frappe
+import erpnext
+from frappe import auth
+import datetime
+import json, ast
+from erpnext.accounts.utils import get_balance_on
+
+@frappe.whitelist()
+def test(doc, method=none):
+    
